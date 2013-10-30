502ab368b6fe586290a2748f81177e85cdbd16cc
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-10-30 12:18:28 -0400

    add priv check to orgs controller

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::OrganizationsController < Api::V1::BaseController
   expose(:organization)
 
+  authorize_resource decent_exposure: true
+
   def show
     respond_with :api, organization
   end
