66577e6fe91574b9d6c092ad79b88750f9090c3b
Andrew Kuklewicz <andrew@beginsinwonder.com>
2013-10-30 12:15:12 -0400

    add in more mailer impl

--- app/mailers/transcript_complete_mailer.rb
+++ app/mailers/transcript_complete_mailer.rb
@@ -1,3 +1,14 @@
 class TranscriptCompleteMailer < ActionMailer::Base
-  default from: ENV['EMAIL_USERNAME']  
+  default from: ENV['EMAIL_USERNAME'], template_path: ['base', 'transcript_complete_mailer']
+
+  def new_auto_transcript(user, audio_file, item)
+    @user, @audio_file, @item = user, audio_file, item
+    mail(to: @user.email, subject: 'Transcription of audio complete')
+  end
+
+  def new_amara_transcript(user, audio_file, item)
+    @user, @audio_file, @item = user, audio_file, item
+    mail(to: @user.email, subject: 'New transcript for audio')
+  end
+
 end
