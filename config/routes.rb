23d9b6109e38518629b125270f67e83f47292d0c
chrisrhoden <=>
2013-11-20 20:56:59 -0500

    fix root redirect

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ PopUpArchive::Application.routes.draw do
 
   match '/*path' => redirect {|params, request| "https://www.popuparchive.org/#{params[:path]}" }, constraints: { host: 'pop-up-archive.herokuapp.com' }
   match '/*path' => redirect {|params, request| "https://www.popuparchive.org/#{params[:path]}" }, constraints: { host: 'beta.popuparchive.org' }
-  match '/*path' => redirect {|params, request| "https://www.popuparchive.org/#{params[:path]}" }, constraints: { host: 'www.popuparchive.org', protocol: "http" }
-  root to: redirect('https://www.popuparchive.org/'), constraints: {host: 'www.popuparchive.org', protocol: 'http' }
+  match '/*path' => redirect {|params, request| "https://www.popuparchive.org/#{params[:path]}" }, constraints: { host: 'www.popuparchive.org', protocol: "http://" }
+  root to: redirect('https://www.popuparchive.org/'), constraints: { host: 'www.popuparchive.org', protocol: 'http://' }
 
   devise_for :users, controllers: { registrations: 'users/registrations', invitations: 'users/invitations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
