0e6571edab694d2be3be27166f27290ad6ac15e5
chrisrhoden <carhoden@gmail.com>
2013-11-21 11:23:14 -0500

    consistent hash syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ PopUpArchive::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'www.popuparchive.org', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'www.popuparchive.org', protocol: 'https' }
   Rails.application.routes.default_url_options = { host: 'www.popuparchive.org', protocol: 'https'  }
 
   require 'autoscaler/sidekiq'
