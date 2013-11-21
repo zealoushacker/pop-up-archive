ee79de6c9a9731d190e014d9d134015c337b9058
chrisrhoden <=>
2013-11-20 19:52:56 -0500

    match root in addition to splat

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,11 @@ PopUpArchive::Application.routes.draw do
     controllers :applications => 'oauth/applications'
   end
 
-  constraints(host: /(?:pop-up-archive.herokuapp.com)|(?:beta.popuparchive.org)/) do
-    match "/*path" => redirect {|params, req| "https://www.popuparchive.org/#{params[:path]}" }
-  end
 
-  constraints(protocol: 'http') do
-    match '/*path' => redirect {|params, request| "https://www.popuparchive.org/#{params[:path]}" }
-  end
+  match '/*path' => redirect {|params, request| "https://www.popuparchive.org/#{params[:path]}" }, constraints: { host: 'pop-up-archive.herokuapp.com' }
+  match '/*path' => redirect {|params, request| "https://www.popuparchive.org/#{params[:path]}" }, constraints: { host: 'beta.popuparchive.org' }
+  match '/*path' => redirect {|params, request| "https://www.popuparchive.org/#{params[:path]}" }, constraints: { host: 'www.popuparchive.org', protocol: "http" }
+  root to: redirect('https://www.popuparchive.org/'), constraints: {host: 'www.popuparchive.org', protocol: 'http' }
 
   devise_for :users, controllers: { registrations: 'users/registrations', invitations: 'users/invitations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
