77e3944a9c963c4db5499aa5af4556ecf6f52954
chrisrhoden <carhoden@gmail.com>
2013-11-21 14:05:25 -0500

    easier to understand adding free trial for PRX members

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,7 @@ class Users::SessionsController < Devise::SessionsController
 	def new
     session.delete(:card_token)
     session.delete(:plan_id)
+    session[:user_return_to] = params[:return_to] if params[:return_to].present?
     super
 	end
 
