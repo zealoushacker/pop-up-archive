e16310e901b7687cbf6e6dabda4cd2ec6baa64bc
baileyspace <bailey.r.smith@gmail.com>
2013-12-20 13:29:17 -0800

    Precompile jplayer js
    I clearly didn’t know about precompiling.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -49,7 +49,7 @@ PopUpArchive::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['directory/base.css', 'directory/application.js', 'login/base.css', 'login.js', 'jplayer.popup.css']
+  config.assets.precompile += ['directory/base.css', 'directory/application.js', 'login/base.css', 'login.js', 'jplayer.popup.css', 'jquery.jplayer.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
