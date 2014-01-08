580d97f26767f4256c6219065c476f375300e678
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-01-08 18:01:38 -0500

    handle invalid transition gracefully spec

--- spec/workers/finish_task_worker_spec.rb
+++ spec/workers/finish_task_worker_spec.rb
@@ -12,4 +12,12 @@ describe FinishTaskWorker do
     @worker.perform(@task.id).should eq true
   end
 
+  it "dupe finish a task raises no error" do
+    @task = FactoryGirl.create :analyze_task
+    @worker = FinishTaskWorker.new
+    Task.should_receive(:find_by_id).and_return(@task)
+    @task.should_receive(:finish!).and_raise(StateMachine::InvalidTransition.new(@task, Task.state_machines[:status], :finish))
+    @worker.perform(@task.id).should eq true
+  end
+
 end
