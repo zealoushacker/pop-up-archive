0055890cffdbd9ac42d84ed99e8a485fff2a7330
baileyspace <bailey.r.smith@gmail.com>
2014-01-23 14:15:00 -0800

    embed player ogg

--- app/controllers/embed_player_controller.rb
+++ app/controllers/embed_player_controller.rb
@@ -4,7 +4,9 @@ class EmbedPlayerController < ApplicationController
     @file_id=params[:file_id]
     @item_id=params[:item_id]
     @collection_id=params[:collection_id]
-    @url=Rails.application.routes.url_helpers.api_item_audio_file_url(@item_id, @file_id)
+    @mp3 = AudioFile.find(params[:file_id]).public_url(extension: :mp3)
+    @ogg = AudioFile.find(params[:file_id]).public_url(extension: :ogg)
   end
 end
+
 
