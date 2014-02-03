85b224e624e33ee25bfc83828b8d8b9aaae85bed
Lasse Christiansen <webdevlasse@gamil.com>
2014-02-03 15:16:53 -0800

    schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140128231229) do
+ActiveRecord::Schema.define(:version => 20140128232541) do
 
   add_extension "hstore"
 
