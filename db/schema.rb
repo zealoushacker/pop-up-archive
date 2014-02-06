adbf960f30c4d272c5e476f0278cbcc3731f1e61
Lasse Christiansen <webdevlasse@gamil.com>
2014-02-05 16:15:42 -0800

    schema change for migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140128232541) do
+ActiveRecord::Schema.define(:version => 20140206001044) do
 
   add_extension "hstore"
 
