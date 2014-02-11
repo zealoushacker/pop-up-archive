2868a4d31aa0d2cd1891d07109152008e6c471e9
lasse christiansen <webdevlasse@gmail.com>
2014-02-11 12:31:56 -0800

    Update 20140206193153_add_listens_to_audio_files.rb

--- db/migrate/20140206193153_add_listens_to_audio_files.rb
+++ db/migrate/20140206193153_add_listens_to_audio_files.rb
@@ -1,6 +1,6 @@
 class AddListensToAudioFiles < ActiveRecord::Migration
   def up
-    add_column :audio_files, :play_count, :integer, :null => false, :default => 1
+    add_column :audio_files, :play_count, :integer, :null => false, :default => 0
   end
 
   def down
