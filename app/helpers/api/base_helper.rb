31fda577d6f653c83b4291e80f5b839a335dac7a
Lasse Christiansen <webdevlasse@gmail.com>
2014-02-18 12:58:44 -0800

    helpers needed

--- app/helpers/api/base_helper.rb
+++ app/helpers/api/base_helper.rb
@@ -10,4 +10,25 @@ module Api::BaseHelper
   def format_time(seconds)
     Time.at(seconds).getgm.strftime('%H:%M:%S')
   end
+
+  def total_hours_on_pua
+    u = User.all
+    total_time = u.map {|user| [user.used_unmetered_storage, user.used_metered_storage] }.flatten.sum 
+    time_definition(total_time)
+  end
+
+  def total_time_in_hours
+    u = User.all
+    total_time = u.map {|user| [user.used_unmetered_storage, user.used_metered_storage] }.flatten.sum
+    hours = (total_time / 3600).to_s + "hrs"
+    return hours
+  end        
+
+  def time_definition(total_time)
+    mm, ss = total_time.divmod(60)            
+    hh, mm = mm.divmod(60)           
+    dd, hh = hh.divmod(24)         
+    time = "%dd: %dh: %dm: %ds" % [dd, hh, mm, ss]
+    return time
+  end  	
 end
