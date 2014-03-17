c7531303409bfa3c87ed2dc652dfbb460a0e1439
Lasse Christiansen <webdevlasse@gmail.com>
2014-03-17 16:10:45 -0700

    after_commit callbacks from a_f.rb in i_f.rb

--- app/models/image_file.rb
+++ app/models/image_file.rb
@@ -7,4 +7,22 @@ class ImageFile < ActiveRecord::Base
 
   mount_uploader :file, ImageUploader
 
+  after_commit :process_file, on: :create
+  after_commit :process_update_file, on: :update
+
+  def process_update_file
+    # logger.debug "af #{id} call copy_to_item_storage"
+    copy_to_item_storage
+  end
+
+  def process_file
+    # don't process file if no file to process yet (s3 upload)
+    return if !has_file? && original_file_url.blank?
+
+    copy_original
+
+  rescue Exception => e
+    logger.error e.message
+    logger.error e.backtrace.join("\n")
+  end
 end
