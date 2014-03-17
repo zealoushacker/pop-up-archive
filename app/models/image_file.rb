d52a791e303341437c2ae70d7dccf0b918ee4325
Lasse Christiansen <webdevlasse@gmail.com>
2014-03-17 15:59:03 -0700

    image_file_spec

--- app/models/image_file.rb
+++ app/models/image_file.rb
@@ -2,7 +2,7 @@ class ImageFile < ActiveRecord::Base
 
   include FileStorage
 
-  attr_accessible :file, :item_id, :original_file_url
+  attr_accessible :file, :item_id, :original_file_url, :storage_id
   belongs_to :item
 
   mount_uploader :file, ImageUploader
