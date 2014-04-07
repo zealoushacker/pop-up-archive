a3f7657257a292d2e9cba2b7f78653575082a9a4
Lasse Christiansen <webdevlasse@gmail.com>
2014-04-07 09:46:35 -0700

    filename method needed in file_storage

--- app/models/file_storage.rb
+++ app/models/file_storage.rb
@@ -147,6 +147,19 @@ module FileStorage
     File.join("/", dir, filename(version))
   end
 
+  def filename(version=nil)
+    fn = if has_file?
+      f = version ? file.send(version) : file
+      File.basename(f.path)
+    elsif !original_file_url.blank?
+      f = URI.parse(original_file_url).path || ''
+      x = File.extname(f)
+      v = !version.blank? ? ".#{version}" : nil
+      File.basename(f, x) + (v || x)
+    end || ''
+    fn
+  end  
+
   def destination_directory(options={})
     stor = options[:storage] || storage
     stor.use_folders? ? stor.bucket : item.token
