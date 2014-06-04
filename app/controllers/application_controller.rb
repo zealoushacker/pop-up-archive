ed5ea4086b99a4d6def8b32fbcdfbff05e421354
Alex Notov <anotov@gmail.com>
2014-06-04 13:32:22 -0600

    [Finished #72644102] Force ssl globally for app except in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  force_ssl if: :ssl_configured?
+
   protect_from_forgery
 
   # decent_configuration do
@@ -8,6 +10,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
+  end
+
+  private
+  def ssl_configured?
+    !Rails.env.development?
   end
 
 end
